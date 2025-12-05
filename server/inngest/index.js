a2fa25c36a3bbff2e4e7b0d8ba05c06ce0197e3a
ethyne2666 <charankumar2666@gmail.com>
2025-12-05 21:58:19 +0530

    feat: initialize server with Prisma, Express, and Inngest integration
    - Added package.json for server dependencies and scripts.
    - Created prisma.config.ts for database configuration.
    - Defined Prisma schema with models for User, Workspace, Project, Task, and Comment.
    - Implemented server.js with Express setup, middleware for Clerk, and Inngest API integration.

---
+++ server/inngest/index.js
@@ -0,0 +1,41 @@
+import { Inngest } from "inngest";
+import prisma from "../configs/prisma.js";
+
+// Create a client to send and receive events
+export const inngest = new Inngest({ id: "project-management" });
+
+// Ingest Function to save user data to a database
+
+const syncUserCreation = inngest.createFunction(
+    {id: 'sync-user-from-clerk'},
+    {event: 'clerk/user.created'},
+    async ( event ) => {
+       const {data} = event
+       await prisma.user.create({
+        data: {
+            id : data.id,
+            email: data?.email_addresses[0] ?.email_addres,
+            name: data?.first_name + " " + data.last_name,
+            image:data?.image_url, 
+        }
+       })
+    }
+)
+
+// Inngest function to delete user from database
+const syncUserDeletion = inngest.createFunction(
+    {id: 'delete-user-with-clerk'},
+    {event: 'clerk/user.deleted'},
+    async ( event ) => {
+       const {data} = event
+       await prisma.user.delete({
+        where: {
+            id: data.id,
+        }
+       })
+    }
+)
+
+
+// Create an empty array where we'll export future Inngest functions
+export const functions = [];
