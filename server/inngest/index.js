0364443271e0b63b5a148b4282471dca9daff4da
ethyne2666 <charankumar2666@gmail.com>
2025-12-05 22:06:10 +0530

    feat: add user update functionality and configure Vercel settings

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -36,6 +36,32 @@ const syncUserDeletion = inngest.createFunction(
     }
 )
 
+//Inngest function to update user data in database
+const syncUserUpdation = inngest.createFunction(
+    {id: 'update-user-from-clerk'},
+    {event: 'clerk/user.updated'},
+    async ( event ) => {
+       const {data} = event
+       await prisma.user.update({
+        where: {
+            id: data.id
+        },
+        data: {
+            
+            email: data?.email_addresses[0] ?.email_addres,
+            name: data?.first_name + " " + data.last_name,
+            image:data?.image_url, 
+        }
+       })
+    }
+)
+
+
 
 // Create an empty array where we'll export future Inngest functions
-export const functions = [];
+export const functions = [
+    syncUserCreation,
+    syncUserDeletion,
+    syncUserUpdation
+
+];
